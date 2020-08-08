5bbc034b50a0cbab3b93735ef9ac958d1c8c0fd4
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 13:15:12 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,27 +1,11 @@
 def count_strings(array)
-  total = 0
-  
   array.count do |ele|
-    
-    if ele.class == String
-      total += 1
-    end
-    
+    ele.class == String
   end
-  
-    total
 end
 
 def count_empty_strings(array)
-  total = 0
-  
   array.count do |ele|
-    
-    if ele == ""
-      total += 1
-    end
-    
+    ele == ""
   end
-  
-  total
 end
